42530d7383172650aade2be9f7bda9259ce80e74
david_mk <david.mkrtchyan@globalit.am>
2019-08-20 09:29:32 +0400

    angular setup, added dashboard module with components, ui-kits and ui-fragments components

---
+++ src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DashboardRoutingModule } from './dashboard-routing.module';
+import { HomeComponent } from './home/home.component';
+import { TeamComponent } from './team/team.component';
+import { ContactComponent } from './contact/contact.component';
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    TeamComponent,
+    ContactComponent
+  ],
+  imports: [
+    CommonModule,
+    DashboardRoutingModule
+  ]
+})
+export class DashboardModule { }
---
+++ src/app/ui-fragments/nav-bar-menu/nav-bar-menu.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavBarMenuComponent } from './nav-bar-menu.component';
+
+
+
+@NgModule({
+  declarations: [NavBarMenuComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class NavBarMenuModule { }
