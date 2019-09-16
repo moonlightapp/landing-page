908e45847a42542d55ff52717a8c4c2a78d64703
david_mk <david.mkrtchyan@globalit.am>
2019-09-16 11:33:51 +0400

    changed phone mock in home page

--- src/assets/images/knack-node-v1/server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const app = require('./src/app');
-
-const port = process.env.PORT || 5000;
-const server = http.createServer(app);
-
-server.listen(port, function () {
-    console.log('Server is listening to - ', port);
-});
