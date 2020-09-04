08db75539a216c791c57abeaf316ab82253cc62b
besarism <b3sar11@gmail.com>
2020-09-04 16:05:33 +0200

    update

--- src/app/constants/home.messages.ts
+++ src/app/constants/home.messages.ts
@@ -1,50 +1,158 @@
 export const HOME_MESSAGES = [
     {
-        title: 'Ola',
-        content: 'Hi',
-        isQuestion: true,
-        isVisible: false
-    },
-    {
-        title: 'John',
-        content: 'Hello',
+        title: 'Mathias',
+        content: 'Aurora, kan du i det minste snakke med meg?',
         isQuestion: false,
         isVisible: false
     },
     {
-        title: 'Ola',
-        content: 'Hvordan går det?',
+        title: 'Aurora',
+        content: 'Slutt å sende meldinger til meg! Jeg kjenner deg ikke.',
         isQuestion: true,
         isVisible: false
     },
     {
-        title: 'Ola',
-        content: 'alt bra?',
-        isQuestion: true,
-        isVisible: false
-    },
-    {
-        title: 'John',
-        content: 'Ja',
+        title: 'Mathias',
+        content: 'Jeg er din ektemann',
         isQuestion: false,
         isVisible: false
     },
     {
-        title: 'John',
-        content: 'Alt bra, takk.',
+        title: 'Mathias',
+        content: 'Jeg er den du elsket',
         isQuestion: false,
         isVisible: false
     },
     {
-        title: 'Ola',
-        content: ':)',
+        title: 'Mathias',
+        content: 'Vi har vært gift i seks år',
+        isQuestion: false,
+        isVisible: false
+    },
+    {
+        title: 'Aurora',
+        content: 'Jeg husker deg ikke.',
         isQuestion: true,
         isVisible: false
     },
     {
-        title: 'Ola',
-        content: 'Hadde :)',
+        title: 'Aurora',
+        content: 'Jeg har aldri vært gift',
         isQuestion: true,
+        isVisible: false
+    },
+    {
+        title: 'Aurora',
+        content: 'Og jeg har definitivt ikke en ektemann',
+        isQuestion: true,
+        isVisible: false
+    },
+    {
+        title: 'Mathias',
+        content: 'Det må være noe du husker om oss',
+        isQuestion: false,
+        isVisible: false
+    },
+    {
+        title: 'Mathias',
+        content: 'Du må vel huske vårt første kyss på balkongen til foreldrene dine.',
+        isQuestion: false,
+        isVisible: false
+    },
+    {
+        title: 'Mathias',
+        content: 'Jeg savnet deg Aurora',
+        isQuestion: false,
+        isVisible: false
+    },
+    {
+        title: 'Mathias',
+        content: 'Du var i koma i en måned',
+        isQuestion: false,
+        isVisible: false
+    },
+    {
+        title: 'Aurora',
+        content: 'Jeg har hørt om det',
+        isQuestion: true,
+        isVisible: false
+    },
+    {
+        title: 'Aurora',
+        content: 'Men jeg ser ikke ut til å huske det',
+        isQuestion: true,
+        isVisible: false
+    },
+    {
+        title: 'Mathias',
+        content: 'Du har hukommelsestap',
+        isQuestion: false,
+        isVisible: false
+    },
+    {
+        title: 'Mathias',
+        content: 'Legen sa at i ditt tillfelle.',
+        isQuestion: false,
+        isVisible: false
+    },
+    {
+        title: 'Mathias',
+        content: 'Trenger ikke hukommelsen å komme tilbake',
+        isQuestion: false,
+        isVisible: false
+    },
+    {
+        title: 'Aurora',
+        content: 'Hvorfor har legen gitt deg mine personlige opplysninger. Jeg skal saksøke sykehuset for taushetsplikt',
+        isQuestion: true,
+        isVisible: false
+    },
+    {
+        title: 'Mathias',
+        content: 'Jeg er din ektemann, det er derfor',
+        isQuestion: false,
+        isVisible: false
+    },
+    {
+        title: 'Mathias',
+        content: 'vær så snill, HUSK OSS',
+        isQuestion: false,
+        isVisible: false
+    },
+    {
+        title: 'Aurora',
+        content: 'Tror du ikke at jeg prøver.',
+        isQuestion: true,
+        isVisible: false
+    },
+    {
+        title: 'Aurora',
+        content: 'Jeg kan ikke huske deg.',
+        isQuestion: true,
+        isVisible: false
+    },
+    {
+        title: 'Aurora',
+        content: 'Eller noe om oss',
+        isQuestion: true,
+        isVisible: false
+    },
+    {
+        title: 'Mathias',
+        content: 'Vær så snill, vi elsker hverandre. Jeg kan ikke miste deg! ',
+        isQuestion: false,
+        isVisible: false
+    },
+    {
+        title: 'Aurora',
+        content: 'Slutt å sende blomster til huset mitt',
+        isQuestion: true,
+        isVisible: false
+    },
+    {
+        title: 'Mathias',
+        content: 'Det er favoritt blomstene dine',
+        isQuestion: false,
         isVisible: false
     },
 ];
